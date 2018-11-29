6ffef72e6bdbe2ccf43324faaef17f543b1f4d31
Oluwafemi Adeniji <femicodes@gmail.com>
2018-11-29 08:07:52 +0100

    set the views upp

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 require('./models/db');
 const express = require('express');
+const employeeController = require('./controllers/employeeController');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
 let app = express();
+
+app.set('views', path.join(__dirname, '/views/'));
+app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts' }));
+app.set('view engine', 'hbs');
+
+app.use('/employee', employeeController);
 
 app.listen(1738, () => {
     console.log('server started on port fetty wap lmao!');
