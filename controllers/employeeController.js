3b506cfb74acd47c4763d36ae3668ec5d1aaa4c4
Oluwafemi Adeniji <femicodes@gmail.com>
2018-11-29 11:14:00 +0100

    added form validation

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -19,17 +19,38 @@ router.post('/', (req, res) => {
 
 let insertRecord = (req, res) => {
       let employee = new Employee();
-    employee.fullName = req.body.fullName;
+    employee.fullName = req.body.fullName; 
     employee.mobile = req.body.mobile;
     employee.city = req.body.city;
     employee.email = req.body.email;
     employee.save((err, doc) => {
         if (!err) {
             res.redirect('employee/list');
+        } else if (err.name == 'ValidationError') {
+            handleValidationError(err, req.body);
+            res.render('employee/addOrEdit', {
+                viewTitle: 'Insert Employee',
+                employee: req.body
+            });
         } else {
             console.log('Error during record insertion: ' + err);
         }
     });
 }
+
+let handleValidationError = (err, body) => {
+    for (field in err.errors) {
+        switch (err.errors[field].path) {
+            case 'fullName':
+                body['fullNameError'] = err.errors[field].message;
+                break;
+            case 'email':
+                body['emailError'] = err.errors[field].message;
+                break;
+            default:
+                break;
+        }
+    }
+} 
 
 module.exports = router;
